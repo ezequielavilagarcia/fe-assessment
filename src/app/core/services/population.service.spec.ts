a38c9d1ba7db21443a8cfcd24cd58bb76f5ffcdd
Ezequiel Avila Garcia <ezequielavilagarcia@gmail.com>
2019-01-13 16:25:51 -0300

    Retrieve data from the URL provided and show it * Added unit tests to retrieve data in population service * Added unit tests to show data in inhabitants page

--- src/app/core/services/population.service.spec.ts
+++ src/app/core/services/population.service.spec.ts
@@ -1,12 +1,109 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+
+import { of, noop } from 'rxjs';
 
 import { PopulationService } from './population.service';
+import { APP_CONFIG, AppConfig } from 'src/app/app.config';
+import { Gnome } from 'src/app/shared/models/gnome';
 
 describe('PopulationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const inhabitantsList: Gnome[] = [
+    {
+      id: 0,
+      name: 'Fake 0',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 30,
+      height: 12,
+      weight: 12,
+      friends: [],
+      hair_color: 'blue'
+    },
+    {
+      id: 1,
+      name: 'Fake 1',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 20,
+      height: 11,
+      weight: 11,
+      friends: [],
+      hair_color: 'pink'
+    },
+    {
+      id: 2,
+      name: 'Fake 2',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 10,
+      height: 10,
+      weight: 10,
+      friends: [],
+      hair_color: 'red'
+    }
+  ];
+  beforeEach(() => {
+    const APP_CONFIGStub: AppConfig = {
+      apiEndpoint: 'fakeApiEndpoint'
+    };
+
+    class HttpClientStub {
+      get() {
+        return of(inhabitantsList);
+      }
+    }
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: APP_CONFIG, useValue: APP_CONFIGStub },
+        { provide: HttpClient, useClass: HttpClientStub }
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: PopulationService = TestBed.get(PopulationService);
     expect(service).toBeTruthy();
+  });
+
+  describe('#loadData', () => {
+
+    it('should call http client get with correct url', () => {
+      const service: PopulationService = TestBed.get(PopulationService);
+      const appConfig: AppConfig = TestBed.get(APP_CONFIG);
+      const http: Partial<HttpClient> = TestBed.get(HttpClient);
+      const httpSpy = spyOn(http, 'get').and.callThrough();
+
+      service.loadData();
+
+      expect(httpSpy).toHaveBeenCalledWith(appConfig.apiEndpoint);
+    });
+
+    it('should do just one http request with multiples calls', () => {
+      const service: PopulationService = TestBed.get(PopulationService);
+      const http: Partial<HttpClient> = TestBed.get(HttpClient);
+      const httpSpy = spyOn(http, 'get').and.callThrough();
+
+      // Load data in memory
+      service.loadData().subscribe(noop);
+      // It doesn't have to call http method here
+      service.loadData().subscribe(noop);
+
+      expect(httpSpy).not.toHaveBeenCalledTimes(1);
+    });
+
+    it('should send the inhabitants list to gnomesInformation$ observable', () => {
+      const fakeResponse: Partial<Gnome>[] = [{ id: 123 }];
+      const service: PopulationService = TestBed.get(PopulationService);
+      const http: Partial<HttpClient> = TestBed.get(HttpClient);
+      spyOn(http, 'get').and.callFake(() => of({ Brastlewark: fakeResponse }));
+
+      service.loadData().subscribe(noop);
+
+      service.gnomesInformation$.subscribe(data => {
+        expect(data[0].id).toBe(fakeResponse[0].id);
+      });
+    });
   });
 });
--- src/app/inhabitants/inhabitants.page.spec.ts
+++ src/app/inhabitants/inhabitants.page.spec.ts
@@ -1,26 +1,116 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+import { of, BehaviorSubject } from 'rxjs';
 
 import { InhabitantsPage } from './inhabitants.page';
+import { PopulationService } from '../core/services/population.service';
+import { Gnome } from '../shared/models/gnome';
 
 describe('InhabitantsPage', () => {
   let component: InhabitantsPage;
   let fixture: ComponentFixture<InhabitantsPage>;
+  const inhabitantsList: Gnome[] = [
+    {
+      id: 0,
+      name: 'Fake 0',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 30,
+      height: 12,
+      weight: 12,
+      friends: [],
+      hair_color: 'blue'
+    },
+    {
+      id: 1,
+      name: 'Fake 1',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 20,
+      height: 11,
+      weight: 11,
+      friends: [],
+      hair_color: 'pink'
+    },
+    {
+      id: 2,
+      name: 'Fake 2',
+      thumbnail: 'fake-thumbnail.jpg',
+      professions: ['prof1', 'prof2'],
+      age: 10,
+      height: 10,
+      weight: 10,
+      friends: [],
+      hair_color: 'red'
+    }
+  ];
 
-  beforeEach(async(() => {
+  beforeEach(() => {
+    class PopulationServiceStub {
+      private gnomesInformationSubject = new BehaviorSubject([]);
+      public gnomesInformation$ = this.gnomesInformationSubject.asObservable();
+      loadData() {
+        this.gnomesInformationSubject.next(inhabitantsList);
+        return of(null);
+      }
+      setGnomeSelected() {}
+    }
+
+    class RouterStub {
+      navigate(valueToNavigate: []) {}
+    }
     TestBed.configureTestingModule({
       declarations: [InhabitantsPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: PopulationService, useClass: PopulationServiceStub },
+        { provide: Router, useClass: RouterStub }
+      ]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(InhabitantsPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to load data on init', () => {
+    const populationService = TestBed.get(PopulationService);
+    const spy = spyOn(populationService, 'loadData').and.callThrough();
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should show a list of inhabitants', () => {
+    fixture.detectChanges();
+
+    const virtualScrollItems = fixture.debugElement.query(By.css('.inhabitants-list')).properties
+      .items;
+    let index = 0;
+    for (const item of virtualScrollItems) {
+      expect(item.id).toBe(inhabitantsList[index++].id);
+    }
+  });
+
+  it('should redirect to detail page on click', () => {
+    // Up to now 01/13/2019, virtualscroll can't be affected by fixture.detectChanges so i can't simulate click on ion-item
+    // It is not rendered. I could use ion-list instead of ion-virtual-scroll but I think that in this case it's better to prioritize the
+    // performance given by ion-virtual-scroll.
+    fixture.detectChanges();
+    const gnome = inhabitantsList[0];
+    const populationService = TestBed.get(PopulationService);
+    const router = TestBed.get(Router);
+
+    spyOn(populationService, 'setGnomeSelected').and.callThrough();
+    const spyRouter = spyOn(router, 'navigate').and.callThrough();
+    component.goToGnomeDetail(gnome);
+    expect(spyRouter).toHaveBeenCalledWith(['/inhabitant-detail', gnome.id]);
   });
 });
